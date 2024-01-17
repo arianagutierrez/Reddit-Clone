ea03efa065634fbee137f681ac3470835b20bf08
ariana gutierrez <carla_ari.97@live.com>
2024-01-17 18:50:11 +0100

    update head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Reddit Clone</title>
+        <link rel="icon" href="public\images\reddit-icon.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
